beb003ab7d619bb0063b1c30b0b49d09c3f308d8
mselleki <mselleki98@gmail.com>
2021-08-20 11:59:53 +0200

    MP Task v1

---
+++ MathProcessingTask.py
@@ -0,0 +1,97 @@
+import itertools
+import time
+from random import randint, choice, shuffle, sample
+import os
+from psychopy import core, visual
+from task_template import TaskTemplate
+
+
+class MathProcessing(TaskTemplate):
+    yes_key_name = "espace"
+    yes_key_code = "space"
+    quit_code = "q"
+    keys = ["a", "p", "space", yes_key_name, quit_code]
+    launch_example = True
+    next = f"Pour passer à l'instruction suivante, appuyez sur la touche {yes_key_name}"
+    instructions = [f"Dans ce mini-jeu, appuyez sur la touche {yes_key_name} si la flèche centralee.",
+                    "S'il-vous-plaît, n'appuyez que lorsqu'on vous le demande ou lors de la tâche",
+                    f"Placez votre index sur la touche espace s'il-vous-plaît",
+                    ]
+    csv_headers = ['no_trial', 'id_candidate', 'result',  'ans_candidate', 'good_ans', 'correct',
+                   'practice', 'reaction_time', 'time_stamp']
+
+    def task(self, no_trial, exp_start_timestamp, trial_start_timestamp, practice=False):
+        keyboard_pressed = False
+        numbers = [str(randint(1, 6)) for i in range(4)]
+        print(numbers)
+        op = ["+", "-", "*"]
+        shuffle(op)
+        op_final = list(itertools.chain.from_iterable(itertools.zip_longest(numbers, op)))
+        op_final.pop(-1)
+        op_final = ''.join(op_final)
+        result = eval(op_final)
+
+        if result >= 5:
+            good_ans = 'a'
+        else:
+            good_ans = 'p'
+
+        self.create_visual_text("+").draw()
+        self.win.flip()
+        core.wait(1)
+        self.create_visual_text(op_final).draw()
+        self.win.flip()
+
+        resp, rt = self.get_response_with_time()
+
+        if resp == good_ans:
+            good_answer = True
+        else:
+            good_answer = False
+
+        self.update_csv(no_trial, self.participant, result, resp, good_ans, good_answer,
+                        practice, round(rt, 2), round(time.time() - exp_start_timestamp, 2))
+        self.create_visual_text("").draw()
+        self.win.flip()
+        rnd_time = randint(8, 14)
+        core.wait(rnd_time * 10 ** -3)
+        if practice:
+            return good_answer
+
+    def example(self, exp_start_timestamp):
+        score = 0
+        example = self.create_visual_text(text='Commençons par un exemple')
+        tutoriel_end = self.create_visual_text(text="Le tutoriel est désormais terminé")
+        example.draw()
+        self.create_visual_text(self.next, pos=(0, -0.4), font_size=0.04).draw()
+        self.win.flip()
+        self.wait_yes()
+        for i in range(3):
+            if self.task(i, exp_start_timestamp, time.time(), True):
+                score += 1
+                self.create_visual_text(f"Bravo ! Vous avez {score}/{i + 1}").draw()
+                self.win.flip()
+                core.wait(2)
+            else:
+                self.create_visual_text(f"Dommage... Vous avez {score}/{i + 1}").draw()
+                self.win.flip()
+                core.wait(2)
+        self.create_visual_text("+").draw()
+        self.win.flip()
+        core.wait(1)
+        results = self.create_visual_text(f"Vous avez obtenu {score}/3")
+        results.draw()
+        self.win.flip()
+        core.wait(5)
+        tutoriel_end.draw()
+        self.win.flip()
+        core.wait(5)
+
+    def quit_experiment(self):
+        exit()
+
+
+if not os.path.isdir("csv"):
+    os.mkdir("csv")
+exp = MathProcessing("csv")
+exp.start()
